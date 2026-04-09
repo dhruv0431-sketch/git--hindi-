dd9c7b23778dd58378ba2403719a1dc087cae707
DHRUV PANDEY <panditdhruv0431@gmail.com>
2026-04-09 11:13:10 +0530

    sec com

--- new lec.py
+++ new lec.py
@@ -1,11 +1,18 @@
 #function 
 #write a program if the number is even count the factorial even and the no is odd count thr factorial odd 
-def  factorial (n): 
-    for i in range (1,n+1):
-        if n%i==0: 
-            print(i,end=" ")
-            return 10 
+# def  factorial (n): 
+#     for i in range (1,n+1):
+#         if n%i==0: 
+#             print(i,end=" ")
+#             return 10 
         
-x=factorial(12) 
-print(x)
-            
+# x=factorial(12) 
+# print(x)
+# 
+# 
+list1=[1,2,3,4,5,6,7,8,9]
+list2=[1,2,3,4,5,6,7,8, 9]
+for i in list1:
+    for j in list2:
+        if i==j:
+            print(i,end=" ")
